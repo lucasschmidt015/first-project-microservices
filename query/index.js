4ad931e191dd64592a412b3ecf9483f86f277785
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-03-01 22:46:07 -0300

    Feat: Implemented a route to recover events that were lost.

--- query/index.js
+++ query/index.js
@@ -1,6 +1,6 @@
 const express =  require('express');
 const bodyParser = require('body-parser');
-// const { randomBytes } = require('crypto');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -10,13 +10,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
         posts[id] = { id, title, comments: [] }
@@ -40,10 +34,27 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listen on 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+        console.log("Processing event: ", event.type);
+        handleEvent(event.type, event.data);
+    }
 });
